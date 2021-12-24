693430a91e098a3ef7ab62f8f0df396a3098dcac
Little Endian <87113727+ksmnetwork@users.noreply.github.com>
2021-12-24 01:11:18 +0100

    Update tube.py

--- tube.py
+++ tube.py
@@ -18,14 +18,17 @@ for video_id in videoListName:
       title = video_id['title']['runs'][0]['text']
       filename = "video-{}.txt".format(video_id['videoId'])
 
-      with open(filename, 'w') as fd:
-        fd.write(video)
-        fd.write("\n")
-        fd.write(title)
-        fd.write("\n")
-        fd.write(codext.encode(title, "braille"))
-        fd.write("\n")
-        fd.write(codext.encode(message, "braille"))
+      # Let't see it action with Git Actions
+      print(video, '\n', title, '\n', codext.encode(title, "braille"), '\n', codext.encode(message, "braille"), '\n\n')
+
+      # with open(filename, 'w') as fd:
+      #   fd.write(video)
+      #   fd.write("\n")
+      #   fd.write(title)
+      #   fd.write("\n")
+      #   fd.write(codext.encode(title, "braille"))
+      #   fd.write("\n")
+      #   fd.write(codext.encode(message, "braille"))
 
     except:
         pass
