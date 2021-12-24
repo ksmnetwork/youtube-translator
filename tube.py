63076c20f4fe950626229ce41b9563df5e153b81
Little Endian <87113727+ksmnetwork@users.noreply.github.com>
2021-12-24 03:51:47 +0100

    Update tube.py

--- tube.py
+++ tube.py
@@ -1,34 +1,30 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import scrapetube
 import codext
+import sys
 
-videoListName = scrapetube.get_channel("UClnw_bcNg4CAzF772qEtq4g")
+if len(sys.argv) > 1:
+  channelId = sys.argv[1]
+else:
+  channelId = "UClnw_bcNg4CAzF772qEtq4g"
 
+videoListName = scrapetube.get_channel(channelId)
 for video_id in videoListName:
-    try:
-      transcript_list = YouTubeTranscriptApi.list_transcripts(video_id['videoId'])
-      transcript = transcript_list.find_generated_transcript(['en']).fetch()
-      dscript = []
-
-      for i in transcript:
-        dscript.append(i['text'])
-        message = ' '.join(dscript)
-
-      video = "https://www.youtube.com/watch?v={}".format(video_id['videoId'])
-      title = video_id['title']['runs'][0]['text']
-      filename = "video-{}.txt".format(video_id['videoId'])
-
-      print(video, '\n', title, '\n', codext.encode(title, "braille"), '\n', codext.encode(message, "braille"), '\n\n')
-
-      with open(filename, 'w') as fd:
-        fd.write(video)
-        fd.write("\n")
-        fd.write(title)
-        fd.write("\n")
-        fd.write(codext.encode(title, "braille"))
-        fd.write("\n")
-        fd.write(codext.encode(message, "braille"))
-        fd.close()
-
-    except:
-        pass
+  try:
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id['videoId'])
+    transcript = transcript_list.find_generated_transcript(['en']).fetch()
+    dscript = []
+    for i in transcript:
+      dscript.append(i['text'])
+      message = ' '.join(dscript)
+    with open("video-{}.txt".format(video_id['videoId']), 'w', encoding='utf-8') as fd:
+      fd.write("https://www.youtube.com/watch?v={}".format(video_id['videoId']))
+      fd.write("\n")
+      fd.write(video_id['title']['runs'][0]['text'])
+      fd.write("\n")
+      fd.write(codext.encode(video_id['title']['runs'][0]['text'], "braille"))
+      fd.write("\n")
+      fd.write(codext.encode(message, "braille"))
+      fd.close()
+  except:
+    pass
